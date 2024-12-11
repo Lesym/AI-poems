36cd6966b175f8867f6d6e990d55c5523a1fb0e6
Lesydia Roseline Mupedziswa <lesydiaroseline@gmail.com>
2024-12-11 08:40:35 +0200

    added basic form handling and added typewritter

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 //alert("GOD");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "God is good",
+    autoStart: true,
+    delay: 100,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-form");
+poemFormElement.addEventListener("submit", generatePoem);
